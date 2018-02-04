8c29fa9afdebdf0c3da6f86a8f11011629389c72
Eric Schwartz <erics273@gmail.com>
2018-02-04 05:05:58 -0500

    removing defualt reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import youniversity from './Youniversity'
 import auth from './Auth'
 import ui from './Ui'
 
 const reducer = combineReducers({
     auth,
     ui,
-    default: youniversity,
     routing: routerReducer
 })
 
