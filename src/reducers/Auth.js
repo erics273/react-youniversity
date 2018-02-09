7891314c90794db0ce48ba2d8dfc896017417b19
Eric Schwartz <erics273@gmail.com>
2018-02-09 17:31:00 -0500

    typo

--- src/reducers/Auth.js
+++ src/reducers/Auth.js
@@ -21,6 +21,6 @@ export default auth
 
 //The Auth reducer is responsible for managing authentication data in the state
 //It's main purpose for this project is to make sure that if we have an authorized
-//user that their details and available in the application state.
+//user that the details are avaialble in the application state.
 
 END SOLUTION */
