4f35b7c2e70b77aa20eb293d442448eb9db1b0f0
Eric Schwartz <erics273@gmail.com>
2018-02-12 01:39:27 -0800

    sf

--- src/actions/Ui.js
+++ src/actions/Ui.js
@@ -1,3 +1,8 @@
+/* START SOLUTION */
 export function hideFlash() {
     return (dispatch) => dispatch({type: 'HIDE_FLASH'});
 }
+/* ELSE
+//This is a placeholder for any actions they may be relevent to UI changes
+//such as hiding alerts or other visual changes to the UI.
+END SOLUTION */
--- src/components/Flash.js
+++ src/components/Flash.js
@@ -49,38 +49,18 @@ import { Alert, Glyphicon, Fade } from 'react-bootstrap';
 class Flash extends Component {
 
     render() {
-        let flashMessage = ""; 
-
-            flashMessage = (
-                <Fade in={this.props.showFlash}>
-                    <Alert bsStyle={this.props.flashType == "error" ? "danger" : "info"} onDismiss={this.props.hideFlash}>
-                        <Glyphicon glyph={this.getGlyphicon(this.props.flashType)} />&nbsp;&nbsp;
-                        {this.props.flashType.toUpperCase()} {this.props.flashMessage}
-                    </Alert>
-                </Fade>
-            )
-
-        return (flashMessage);
+        //This should render the flash message if one is returned in the result of our action
+        //Our UI reducer is responsible for putting this information in state when 
+        //an action is dispatched. The data is supplied to this compoent through the container
+        //that renders it.
+        //
+        //Below is some sample react-bootstrap JSX that will render an alert
+        //
+        //     <Alert bsStyle="danger">
+        //          some alert message
+        //     </Alert>            
     }
 
-    getGlyphicon(flashType){
-        let glyph;
-        switch(flashType){
-            case 'error':
-                glyph = "alert";
-                break;
-            case "success":
-                glyph = "ok-sign";
-                break;
-            case "info":
-            case "warning":
-                glyph = "info-sign";
-                break;
-            default:
-                glyph = "";
-        }
-        return glyph;
-    }
 }
 
 export default Flash;
