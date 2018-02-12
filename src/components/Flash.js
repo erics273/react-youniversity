059ce870c06b9b28ae5232027449f3e56b4827f8
Eric Schwartz <erics273@gmail.com>
2018-02-12 01:24:12 -0800

    sadf

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -1,3 +1,47 @@
+/* START SOLUTION */
+import React, { Component } from 'react';
+
+import { Alert, Glyphicon, Fade } from 'react-bootstrap';
+
+class Flash extends Component {
+
+    render() {
+        let flashMessage = ""; 
+
+            flashMessage = (
+                <Fade in={this.props.showFlash}>
+                    <Alert bsStyle={this.props.flashType === "error" ? "danger" : "info"} onDismiss={this.props.hideFlash}>
+                        <Glyphicon glyph={this.getGlyphicon(this.props.flashType)} />&nbsp;&nbsp;
+                        {this.props.flashType.toUpperCase()} {this.props.flashMessage}
+                    </Alert>
+                </Fade>
+            )
+
+        return (flashMessage);
+    }
+
+    getGlyphicon(flashType){
+        let glyph;
+        switch(flashType){
+            case 'error':
+                glyph = "alert";
+                break;
+            case "success":
+                glyph = "ok-sign";
+                break;
+            case "info":
+            case "warning":
+                glyph = "info-sign";
+                break;
+            default:
+                glyph = "";
+        }
+        return glyph;
+    }
+}
+
+export default Flash;
+/* ELSE
 import React, { Component } from 'react';
 
 import { Alert, Glyphicon, Fade } from 'react-bootstrap';
@@ -40,3 +84,4 @@ class Flash extends Component {
 }
 
 export default Flash;
+END SOLUTION */
